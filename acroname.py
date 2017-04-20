1e99de6009c293e70f6d352e2ec6077f20605c4b
Ryan Thomas Correia Ortega <ryan@theagile.engineer>
2017-04-20 00:07:51 -0700

    improving error handling and use of user led
    per justin from acroname I'm adding a try/catch and a few if conditions to help improve the reliability of connecting to hub

--- acroname.py
+++ acroname.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import optparse
 import brainstem
+import sys
 
 def main():
   p = optparse.OptionParser()
@@ -11,24 +12,59 @@ def main():
 
   options, arguments = p.parse_args()
 
+  #connect to the USBHub3+
   stem = brainstem.stem.USBHub3p()
-  stem.discoverAndConnect(brainstem.link.Spec.USB)
 
+  #retry connecting to hub
+  for i in range(0,100):
+    while True:
+        try:
+			result = stem.discoverAndConnect(brainstem.link.Spec.USB)
+			if(result != brainstem.result.Result.NO_ERROR):
+  			      from time import sleep
+  			      sleep(0.2)
+			else:
+			      break
+        except (result != brainstem.result.Result.NO_ERROR):
+           continue
+
+  # turn on the user led for a nice visual indicator that we're talking
+  # to the USBHub3+
+  stem.system.setLED(1)
+
+  # do the user requested action and log any brainstem errors
   if options.enable == True:
-      print 'Enabling Hub Port: %s' % options.port
-      stem.usb.setPortEnable(options.port)
+      sys.stdout.write('Enabling Hub Port: %s' % options.port)
+      result = stem.usb.setPortEnable(options.port)
+      if(result != brainstem.result.Result.NO_ERROR):
+            sys.stdout.write(' failed with error %d' % result)
   elif options.disable == True:
-      print 'Disable Hub Port: %s' % options.port
-      stem.usb.setPortDisable(options.port)
+      sys.stdout.write('Disable Hub Port: %s' % options.port)
+      result = stem.usb.setPortDisable(options.port)
+      if(result != brainstem.result.Result.NO_ERROR):
+            sys.stdout.write(' failed with error %d' % result)
   elif options.cycle == True:
-      print 'Cycling Hub Port: %s' % options.port
-      stem.usb.setPortDisable(options.port)
+      sys.stdout.write('Cycling Hub Port: %s' % options.port)
+      sys.stdout.flush()
+
+      result = stem.usb.setPortDisable(options.port)
+      if(result != brainstem.result.Result.NO_ERROR):
+            sys.stdout.write(' disabling failed with error %d' % result)
       from time import sleep
       sleep(2)
-      stem.usb.setPortEnable(options.port)
+
+      result = stem.usb.setPortEnable(options.port)
+      if(result != brainstem.result.Result.NO_ERROR):
+            sys.stdout.write(' enabling failed with error %d' % result)
   else:
     print 'You must specify a port state with the --enable, --disable, or --cycle option'
 
+  # clear out any stdout buffer and make a new line
+  sys.stdout.flush()
+  print("");
+
+  #turn off the user led to show we're done
+  stem.system.setLED(0)
   stem.disconnect()
 
 if __name__ == '__main__':
